977fdb4a988d9d3c754b3323c0aaf4bd7233641f
cris-fernandez <cristopherfernandezdeblas@gmail.com>
2025-04-30 10:40:11 +0200

    Calculating how many plots per sp we have

--- 03_scripts/01_01_study_plots_info.R
+++ 03_scripts/01_01_study_plots_info.R
@@ -1,15 +1,34 @@
 rm(list=ls()) #Clearing Gl environment
 
 pck<- c("tidyverse", "dplyr", "patchwork", "grid", "easyclimate",
-        "ggprism") #list of packages
+        "ggprism", "forcats") #list of packages
 new_pck <- pck[!(pck %in% installed.packages()[,"Package"])] #new packages (not installed ones)
 if(length(new_pck)) install.packages(new_pck) #install new packages
 lapply(pck, library, character.only=T) #load all packages
 
-setwd("C:/Users/recup/Desktop/TESIS/FOREST_SYSTEMS")
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 setwd('..')
 getwd()
 
-IBF_data <- read.csv("FOREST_SYSTEMS/00_IB_ForRes_plot_data.csv", header = T) %>% 
-  dplyr::select(-X)
-summary(IBF_data$dominance)
+# 1.- Reading target data ####
+
+clean_target <- read.csv("C:/Users/recup/Universidad de Alcala/IBFORRES/git_local_ibforres/Database_IBFORRES/05_outputs/03_03_result_target.csv", 
+                         header = T, sep = ",") %>% select(-X) %>% 
+  mutate(site = substr(plot_id, 1, 3))
+
+# 2.- Number of plots per species ####
+
+psylv <- clean_target %>% 
+  select(c(plot_id, sp_id)) %>% 
+  filter(sp_id == "Pinsylv") %>% 
+  unique()
+ppine <- clean_target %>% 
+  select(c(plot_id, sp_id)) %>% 
+  filter(sp_id == "Pinpine") %>% 
+  unique()
+aalba <- clean_target %>% 
+  select(c(plot_id, sp_id)) %>% 
+  filter(sp_id == "Abialba") %>% 
+  unique() 
+# We have 52 P. sylvestris, 17 P. pinea and 23 A. alba plots :)
+
