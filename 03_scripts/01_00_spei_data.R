cec77fa3fab287198ebf8d709df81546e42e248f
cris-fernandez <cristopherfernandezdeblas@gmail.com>
2025-05-05 13:05:13 +0200

    New script for spei data wrangling

---
+++ 03_scripts/01_00_spei_data.R
@@ -0,0 +1,14 @@
+rm(list=ls()) #Clearing Gl environment
+
+pck<- c("tidyverse", "dplyr", "patchwork", "grid", "easyclimate",
+        "ggprism", "forcats", "raster") #list of packages
+new_pck <- pck[!(pck %in% installed.packages()[,"Package"])] #new packages (not installed ones)
+if(length(new_pck)) install.packages(new_pck) #install new packages
+lapply(pck, library, character.only=T) #load all packages
+
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+setwd('..')
+getwd()
+
+
+r = raster("data/nc_3B42.20060101.03.7A.HDF.Z.ncml.nc")
