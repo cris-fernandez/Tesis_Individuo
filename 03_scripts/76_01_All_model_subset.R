abfa579d1bf6ffc3975f62bbbde3c22424991da4
cris-fernandez <cristopherfernandezdeblas@gmail.com>
2026-02-09 19:58:07 +0100

    Buenos días estoy loca y me voy a suicidar

---
+++ 03_scripts/76_01_All_model_subset.R
@@ -0,0 +1,157 @@
+rm(list=ls()) #Clearing Gl environment
+
+pck<- c("tidyverse", "dplyr", "patchwork", "grid", "easyclimate",
+        "ggprism", "forcats", "GGally", "MuMIn", "corrr", "ggcorrplot","ggfortify", 
+        "FactoMineR", "factoextra", "ggplot2", "ggbiplot", "ggfortify", "MASS", 
+        "viridis", "vegan", "stats", "devtools", "lavaan", "tidySEM") #list of packages
+new_pck <- pck[!(pck %in% installed.packages()[,"Package"])] #new packages (not installed ones)
+if(length(new_pck)) install.packages(new_pck) #install new packages
+lapply(pck, library, character.only=T) #load all packages
+
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+setwd('..')
+getwd()
+
+# install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+library(pairwiseAdonis)
+
+# 1.- Reading target data ####
+
+clean_target <- read.csv("C:/Users/recup/Universidad de Alcala/IBFORRES/git_local_ibforres/Database_IBFORRES/05_outputs/03_03_result_target.csv",
+                         header = T, sep = ",") %>% dplyr::select(-X) %>%
+  mutate(site = substr(plot_id, 1, 3))
+
+# 2.- Removing 2023 data ####
+# So I can have in the same column 2022 and 2023 values
+
+clean_target <- clean_target %>% 
+  dplyr::select(-contains("_23"))
+
+# Adding T290 defoliation info:
+
+clean_target <- clean_target %>% 
+  mutate(mean_def_obs = ifelse(tree_number == "T290", 15, mean_def_obs))
+
+# 3.- Additional IDs ####
+
+clean_target$pair_id <- ifelse(grepl("NAV|PEL", clean_target$plot_id) == T, "Mad-Pinpine",
+                               ifelse(grepl("GUA", clean_target$plot_id) == T, "Mad-Pinsylv",
+                                      ifelse(grepl("ADO|TRA|ALU", clean_target$plot_id) == T, "Gua-Pinsylv",
+                                             ifelse(grepl("COR|CED", clean_target$plot_id) == T, "Ter-Pinsylv",
+                                                    ifelse(grepl("RON|URZ", clean_target$plot_id) == T, "Nav-Pinsylv",
+                                                           ifelse(grepl("BAS|SAR", clean_target$plot_id) == T, "Nav-Abialba",
+                                                                  ifelse(grepl("FAG|OZA", clean_target$plot_id) == T, "Hue-Abialba",
+                                                                         "z")))))))
+
+clean_target$vigor_id <- ifelse(clean_target$spot_status == "coldspot",
+                                "cold_healthy",
+                                ifelse(clean_target$mean_def_obs < 25,
+                                       "hot_healthy", "hot_damaged")) %>% 
+  as.factor()
+
+# 4.- Data corrections #####
+
+clean_target$total_chl_fw_22 <- ifelse(clean_target$total_chl_fw_22 > 3000, NA,
+                                       clean_target$total_chl_fw_22)
+clean_target$xc_fw_22 <- ifelse(clean_target$xc_fw_22 > 2000, NA,
+                                ifelse(clean_target$total_chl_fw_22 < 0, 
+                                       NA, clean_target$xc_fw_22))
+clean_target$chl_xc_22 <- ifelse(clean_target$chl_xc_22 < 0, NA,
+                                 clean_target$chl_xc_22)
+clean_target$chla_chlb_22 <- ifelse(clean_target$chla_chlb_22 < 0, NA,
+                                    clean_target$chla_chlb_22)
+
+clean_target$sp_id <- ifelse(clean_target$tree_number == "missing_1" | 
+                               clean_target$tree_number == "missing_2",
+                             "Pinsylv", clean_target$sp_id)
+
+clean_target <- clean_target %>% 
+  mutate(sp_id = factor(sp_id))
+
+clean_target <- clean_target[!is.na(clean_target$sp_id), ]
+
+clean_target <- clean_target %>%
+  mutate(sp_id = fct_relevel(sp_id, "Abialba", "Pinsylv", "Pinpine"),
+         vigor_id = fct_relevel(vigor_id, "cold_healthy", "hot_healthy", "hot_damaged"),
+         spot_status = fct_relevel(spot_status, "coldspot", "hotspot")) %>% 
+  filter(mean_def_obs < 100)
+
+
+# 5.- Selecting variables ####
+
+clean_target <- clean_target %>% 
+  dplyr::select(c(height, total_chl_fw_22, percent_n, leaf_d13c, leaf_d18o_corrected,
+                  sla_22, xc_fw_22,mean_1980, mean_def_obs, tree_number, sp_id, spot_status, vigor_id,
+                  pair_id))
+
+summary(clean_target)
+# 112 Aa, 268 Ps, 85 Pp
+levels(clean_target$spot_status) # Coldspot first
+
+
+# 6.- Filtering and standardising ####
+# Standardization by site
+clean_target <- clean_target %>% 
+  group_by(pair_id) %>% 
+  mutate(across(where(is.numeric), scale),
+         mean_def_obs = ifelse(spot_status == "coldspot", 0, mean_def_obs))
+
+# Creating dataframes per species for further analyses
+
+aa_target <- clean_target %>% filter(sp_id == "Abialba")
+ps_target <- clean_target %>% filter(sp_id == "Pinsylv")
+pp_target <- clean_target %>% filter(sp_id == "Pinpine")
+
+# 7.- SEM structure ####
+
+sem_model <- '
+mean_1980 ~ height + sla_22
+leaf_d13c ~ sla_22 + height + mean_1980
+mean_def_obs ~ sla_22 + height + mean_1980 + leaf_d13c
+'
+
+# 8.- Multigroup SEM #
+# The arguments provide the standardized coefficients (useful to compare) and 
+# the R2 values
+
+## 8.1.-  MLR ####
+# Since my variables are not normal, the estimator "RML" assesses that violation 
+# of the normality presumption
+
+# For the bootstraping, subsets must be c. 80% of the size of the original database
+# 0.8 * 112 = 89.6 Abies alba
+# 0.8 * 268 = 214.4 Pinus sylvestris
+# 0.8 * 85 = 68 Pinus pinea
+
+sem_list <- list()
+
+for (i in 1:999) {
+  aa_subset <- aa_target %>% dplyr::slice_sample(n = 89)
+  ps_subset <- ps_target %>% dplyr::slice_sample(n = 214)
+  pp_subset <- pp_target %>% dplyr::slice_sample(n = 68)
+  
+  boot_target <- bind_rows(aa_subset, ps_subset, pp_subset)
+  
+  sem_list[[i]] <- mlr_sem <- sem(sem_model,
+                                  boot_target,
+                                  group = "sp_id",
+                                  estimator = "MLR",
+                                  missing   = "fiml",
+                                  fixed.x = F)
+  print(i)
+}
+
+# Extracting parameters
+
+fit_list <- list()
+for (i in 1:999) {
+  fit_list[[i]] <- parameterEstimates(sem_list[[i]], standardized = TRUE) %>% 
+    dplyr::select(c(lhs, op, rhs, block, group, pvalue, std.all))
+  
+  print(i)
+}
+
+# 
+# summary(mlr_sem, standardized = TRUE, fit.measures = TRUE)
+# Okay, so the SEM is actually robust to the violation of normality as the coefficients
+# remain constant :)
